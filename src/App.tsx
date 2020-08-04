869b28f25fc83a1e386c1102d4ddf971ea7b62af
felipesanderp <felipe.panisson@hotmail.com>
2020-08-04 20:48:31 -0300

    add globalStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import GlobalStyles from './styles/global';
+import Header from './components/Header';
+import Profile from './pages/Profile';
+import Repo from './pages/Repo';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Profile />} />
+
+        <Route path="/:username" element={<Profile />} />
+        <Route path="/:username/:reponame" element={<Repo />} />
+
+      </Routes>
+
+      { /* <Footer /> */ }
+      <GlobalStyles />
+    </BrowserRouter>
   );
 }
 
