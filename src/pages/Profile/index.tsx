da1bef435fc9e5ccc1937babb9064b34456ada38
felipesanderp <felipe.panisson@hotmail.com>
2020-08-06 19:06:40 -0300

    styled profile page mobile first

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,9 +1,50 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, Main, LeftSide, RightSide, Repos } from './styles';
+
+import ProfileData from '../../components/ProfileData';
+import RepoCard from '../../components/RepoCard';
 
 const Profile: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Main>
+        <LeftSide>
+          <ProfileData
+            username={'felipesanderp'}
+            name={'Felipe Sander'}
+            avatarUrl={'https://avatars2.githubusercontent.com/u/46326536?s=460&u=f72b94042e77899f6c4df3c5f41c9984456cda87&v=4'}
+            followers={887}
+            following={7}
+            company={'Nubank'}
+            location={'Curitiba, Brazil'}
+            email={'felipe.panisson@hotmail.com'}
+            blog={undefined}
+          />
+        </LeftSide>
+
+        <RightSide>
+          <Repos>
+            <h2>Random repos</h2>
+
+            <div>
+              {[1, 2, 3, 4, 5, 6].map(n => (
+                <RepoCard
+                  key={n}
+                  username={'felipesanderp'}
+                  reponame={'ecoleta'}
+                  description={'NextLevel Week #01 project'}
+                  language={n % 3 === 0 ? 'JavaScript': 'TypeScript'}
+                  stars={10}
+                  forks={5}
+                />
+              ))}
+            </div>
+          </Repos>
+        </RightSide>
+      </Main>
+    </Container>
+  );
 }
 
 export default Profile;
