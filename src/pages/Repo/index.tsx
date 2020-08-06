fe094e5feecdd5a9799f1127f6128eda6ebb6a84
felipesanderp <felipe.panisson@hotmail.com>
2020-08-06 20:43:50 -0300

    add Repo page

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,9 +1,54 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Breadcrump,
+  RepoIcon,
+  Stats,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  Githubicon,
+} from './styles';
 
 const Repo: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Breadcrump>
+        <RepoIcon />
+        <Link className={'username'} to={'/felipesanderp'}>
+          felipesanderp
+        </Link>
+
+        <span>/</span>
+
+        <Link className={'reponame'} to={'/felipesanderp/ecoleta'}>
+          ecoleta
+        </Link>
+      </Breadcrump>
+
+      <p>NextLevelWeek #01</p>
+
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>stars</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>4</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href={'https://github.com/felipesanderp/ecoleta'}>
+        <Githubicon />
+        <span>View on Github</span>
+      </LinkButton>
+    </Container>
+  );
 }
 
 export default Repo;
