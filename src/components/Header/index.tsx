424b438646688f568183834b511ad17995ef85bc
felipesanderp <felipe.panisson@hotmail.com>
2020-08-06 23:28:09 -0300

    search user from form

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { Container, GithubLogo, SearchForm } from './styles';
 
 const Header: React.FC = () => {
+  const [search, setSearch] = useState('');
+  const navigate = useNavigate();
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    navigate('/' + search.toLowerCase().trim());
+
+    setSearch('');
+  }
+
   return (
     <Container>
       <GithubLogo />
-      <SearchForm>
-        <input type="text" placeholder="Enter Username or Repo..." />
+      <SearchForm onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter Username or Repo..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
       </SearchForm>
     </Container>
   );
