6e4c5997642d215382796ceac20cc947c91cdc26
felipesanderp <felipe.panisson@hotmail.com>
2020-08-07 00:15:20 -0300

    get repo info from github api

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
 
 import {
   Container,
@@ -12,38 +12,68 @@ import {
   Githubicon,
 } from './styles';
 
+import { APIRepo } from '../../@types';
+
+interface Data {
+  repo?: APIRepo;
+  error?: string;
+}
+
 const Repo: React.FC = () => {
+  const { username, reponame  } = useParams();
+  const [data, setData] = useState<Data>();
+
+  useEffect(() => {
+    fetch(`https://api.github.com/repos/${username}/${reponame}`).then(
+      async (response) => {
+        setData(
+          response.status === 404
+            ? { error: 'Repository not found!' }
+            : { repo: await response.json() }
+        );
+      }
+    )
+  }, [username, reponame]);
+
+  if (data?.error) {
+    return <h1>{data.error}</h1>;
+  }
+
+  if (!data?.repo) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
     <Container>
       <Breadcrump>
         <RepoIcon />
-        <Link className={'username'} to={'/felipesanderp'}>
-          felipesanderp
+        <Link className={'username'} to={`${username}`}>
+          {username}
         </Link>
 
         <span>/</span>
 
-        <Link className={'reponame'} to={'/felipesanderp/ecoleta'}>
-          ecoleta
+        <Link className={'reponame'} to={`${username}/${reponame}`}>
+          {reponame}
         </Link>
       </Breadcrump>
 
-      <p>NextLevelWeek #01</p>
+      <p>{data.repo.description}</p>
 
       <Stats>
         <li>
           <StarIcon />
-          <b>9</b>
+          <b>{data.repo.stargazers_count}</b>
           <span>stars</span>
         </li>
         <li>
           <ForkIcon />
-          <b>4</b>
+          <b>{data.repo.forks}</b>
           <span>forks</span>
         </li>
       </Stats>
 
-      <LinkButton href={'https://github.com/felipesanderp/ecoleta'}>
+      <LinkButton href={data.repo.html_url}>
         <Githubicon />
         <span>View on Github</span>
       </LinkButton>
